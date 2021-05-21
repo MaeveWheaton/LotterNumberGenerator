9515ac1ce895c8c52146d725a5423d933a4b853f
MaeveWheaton <teamheaven10@outlook.com>
2021-05-21 14:11:39 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace LotterNumberGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.numberOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLabel.BackColor = System.Drawing.Color.Firebrick;
+            this.titleLabel.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-1, -3);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(942, 165);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Lottery Number Generator";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Mongolian Baiti", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(157, 221);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(601, 72);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = "Press the button below to generate your \r\nwinning 6/49 lottery numbers";
+            this.instructionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // generateButton
+            // 
+            this.generateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.generateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.generateButton.Font = new System.Drawing.Font("Mongolian Baiti", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(320, 340);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(264, 74);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // numberOutput
+            // 
+            this.numberOutput.BackColor = System.Drawing.Color.Black;
+            this.numberOutput.Font = new System.Drawing.Font("Mongolian Baiti", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberOutput.ForeColor = System.Drawing.Color.White;
+            this.numberOutput.Location = new System.Drawing.Point(157, 490);
+            this.numberOutput.Name = "numberOutput";
+            this.numberOutput.Size = new System.Drawing.Size(601, 103);
+            this.numberOutput.TabIndex = 3;
+            this.numberOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(240F, 240F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(941, 782);
+            this.Controls.Add(this.numberOutput);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "King Lottery";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label numberOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotterNumberGenerator
+{
+    public partial class Form1 : Form
+    {
+        Random randGen = new Random();
+        int max = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            //6 variable numbers
+            int num1 = randGen.Next(1, max);
+            int num2;
+            int num3;
+            int num4;
+            int num5;
+            int num6;
+
+            //random generate 6 different numbers, 1-49 ("max" 50)
+            do
+            {
+                num2 = randGen.Next(1, max);
+            }
+            while (num2 == num1);
+            do
+            {
+                num3 = randGen.Next(1, max);
+            }
+            while (num3 == num1 || num3 == num2);
+            do
+            {
+                num4 = randGen.Next(1, max);
+            }
+            while (num4 == num1 || num4 == num2 || num4 == num3);
+            do
+            {
+                num5 = randGen.Next(1, max);
+            }
+            while (num5 == num1 || num5 == num2 || num5 == num3 || num5 == num4);
+            do
+            {
+                num6 = randGen.Next(1, max);
+            }
+            while (num6 == num1 || num6 == num2 || num6 == num3 || num6 == num4 || num6 == num5);
+
+            numberOutput.Text = $"{num1} {num2} {num3} {num4} {num5} {num6}";
+        }
+    }
+}
